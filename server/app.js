b4067035e598bc07d19e3ca4fb845b0eb5a7175f
srujana <srujanapenugonda1318@outlook.com>
2024-01-20 14:58:25 +0530

    feat: update DB Logic

--- server/app.js
+++ server/app.js
@@ -1,35 +1,26 @@
 // app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressionsRouter = require('./routes/expressions');
-const db = require('./models/db');
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressionsRouter = require('./routes/expressions')
+const { db } = require('./models/db')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(bodyParser.json());
-
-// PostgreSQL connection settings
-const pool = new Pool({
-  user: 'postgres-username',
-  host: 'postgres-host',
-  database: 'postgres-database',
-  password: 'postgres-password',
-  port: 5432, // Default PostgreSQL port
-});
+app.use(bodyParser.json())
 
 // Connect to PostgreSQL database
-pool.connect((err) => {
+db.connect((err) => {
   if (err) {
-    console.error('Error connecting to PostgreSQL:', err);
-    return;
+    console.error('Error connecting to PostgreSQL:', err)
+    return
   }
-  console.log('Connected to PostgreSQL database');
-});
+  console.log('Connected to PostgreSQL database')
+})
 
 // Use expressions router
-app.use('/api', expressionsRouter);
+app.use('/api', expressionsRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
--- server/models/db.js
+++ server/models/db.js
@@ -1,30 +1,36 @@
 // models/db.js
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 
 // PostgreSQL connection settings
 const pool = new Pool({
-  user: 'postgres-username',
-  host: 'postgres-host',
-  database: 'postgres-database',
-  password: 'postgres-password',
-  port: 5432, // Default PostgreSQL port
-});
+  user: 'postgres',
+  host: 'database-metrics.c5o8uscasykf.eu-north-1.rds.amazonaws.com',
+  database: 'metricsData',
+  password: 'Admin1234567',
+  port: '5432',
+})
 
-// Function to query the database
-const query = async (text, params) => {
-  const start = Date.now();
-  const client = await pool.connect();
+pool
+  .connect()
+  .then(() => {
+    console.log('Connected to the database successfully!')
+  })
+  .catch((error) => console.log('Error in connecting', error))
 
-  try {
-    const result = await client.query(text, params);
-    const duration = Date.now() - start;
-    console.log('Executed query', { text, duration, rows: result.rowCount });
-    return result;
-  } finally {
-    client.release();
-  }
-};
+// // Function to query the database
+// const query = async (text, params) => {
+//   const start = Date.now()
+
+//   try {
+//     const result = await client.query(text, params)
+//     const duration = Date.now() - start
+//     console.log('Executed query', { text, duration, rows: result.rowCount })
+//     return result
+//   } finally {
+//     client.release()
+//   }
+// }
 
 module.exports = {
-  query,
-};
+  db: pool,
+}
